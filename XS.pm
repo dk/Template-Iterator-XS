862a77ff1dd8ad9a23b761e85a120878aaa5f8a7
Dmitry Karasik <dmitry@karasik.eu.org>
2012-11-16 08:51:59 +0100

    - add pod

--- XS.pm
+++ XS.pm
@@ -12,3 +12,33 @@ our @ISA = qw( Template::Iterator DynaLoader );
 bootstrap Template::Iterator::XS $VERSION;
 
 1;
+
+=pod
+
+=head1 NAME
+
+Template::Iterator::XS - speedup TT2's Iterator
+
+=head1 DESCRIPTION
+
+The module is intended to function as a drop-in replacement for TT's native
+Template::Iterator, for speedup. There is an existing mechanism for a similar
+module, Template::Stash::XS which does the same, and can be selected during
+configuration. The hope for this module is to function exacly in the same fashion.
+
+=head1 USAGE
+
+XXX tbd
+
+=head1 AUTHOR
+
+Dmitry Karasik <dmitry@karasik.eu.org>
+
+=head1 LICENCE
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+The work is sponsored by L<reg.ru> .
+
+=cut
